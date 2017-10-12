226eabecddeafe7f3e99dde662766513bade4dd3
petrkotov <30962854+petrkotov@users.noreply.github.com>
2017-10-12 06:16:50 +0300

    Add files via upload

---
+++ js/calendar.js
@@ -0,0 +1,23 @@
+var openCalendar = document.querySelector(".calendar-button");
+var calendar = document.querySelector(".calendar-window");
+var opened = document.querySelector(".opened-calendar");
+var closed = document.querySelector(".closed-calendar");
+var checker = 0;
+openCalendar.addEventListener("click", function(event){
+if (checker == 0) {
+  event.preventDefault();
+  calendar.classList.add("calendar-window-show");
+  calendar.classList.add("opened-calendar");
+  checker = 1;
+}
+else {
+  event.preventDefault();
+  setTimeout(function() {
+  calendar.classList.remove("calendar-window-show");}, 600);
+  calendar.classList.add("closed-calendar");
+  setTimeout(function() {
+    calendar.classList.remove("closed-calendar");}, 600);
+
+  checker = 0;
+}
+});
